9ed0a894434cf865fac2b4c054b5be913138d7b9
Asmaa Hariti <asmaa@Asmaas-MacBook-Air.local>
2024-09-30 11:29:05 +0100

    entering an Integer then keep summing its reverse until reaching a palindrome

---
+++ Implementations/PalindromeDigitImpl.java
@@ -0,0 +1,32 @@
+package hariti.asmaa.problesolving.Implementations;
+
+import hariti.asmaa.problesolving.PalindromeDigit;
+
+public class PalindromeDigitImpl implements PalindromeDigit {
+
+
+
+    @Override
+    public Integer reverse(Integer number, Integer reverse) {
+        if (number == 0)
+            return reverse;
+
+        reverse = reverse * 10 + number % 10;
+        return reverse(number / 10, reverse);    }
+    @Override
+    public boolean isPalindrome(Integer number) {
+        return number.equals(reverse(number, 0));
+    }
+
+
+    @Override
+    public Integer sum(Integer number) {
+        while (!isPalindrome(number)) {
+            Integer reversed = reverse(number, 0);
+            number = number + reversed;
+        }
+        return number;
+    }
+
+
+}
---
+++ PalindromeDigit.java
@@ -0,0 +1,7 @@
+package hariti.asmaa.problesolving;
+
+public interface PalindromeDigit {
+    Integer reverse(Integer number , Integer reverse);
+    boolean isPalindrome(Integer number);
+    Integer sum(Integer number);
+}
